bef11e617cec4f130f422605c087d3203282909b
Johsua_Kim <joshuara7235@gmail.com>
2020-12-20 20:49:23 +0900

    fix clock

--- project_momentum/clock.js
+++ project_momentum/clock.js
@@ -6,7 +6,9 @@ function getTime () {
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${minutes} : ${seconds < 10 ? `0${seconds}` : seconds}`;
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${
+        minutes <10 ? `0${minutes}` : minutes} : ${
+            seconds < 10 ? `0${seconds}` : seconds}`;
     
 }
 
